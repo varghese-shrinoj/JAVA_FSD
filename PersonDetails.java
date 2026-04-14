7b135856c687d220b3dccd78d02ae4251072889c
Varghese <vargheseshrinoj@gmail.com>
2026-04-14 10:09:36 +0530

    Initial commit

---
+++ PersonDetails.java
@@ -0,0 +1,21 @@
+package pac1;
+
+public class PersonDetails {
+
+public static void main(String[] args) {
+
+String firstName = "Divya";
+String lastName = "Bharathi";
+char gender = 'F';
+int age = 20;
+double weight = 85.55;
+
+System.out.println("Person Details:");
+System.out.println("---------------");
+System.out.println("First Name: " + firstName);
+System.out.println("Last Name: " + lastName);
+System.out.println("Gender: " + gender);
+System.out.println("Age: " + age);
+System.out.println("Weight: " + weight);
+}
+}
